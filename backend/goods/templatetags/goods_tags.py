6e81bd67cecbf58b6d89e47eefa351110d43e3b9
igoroshust <igoroshust75@gmail.com>
2026-02-09 23:04:07 +0900

    feat: search

--- backend/goods/templatetags/goods_tags.py
+++ backend/goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 from goods.models import Categories
 
 # Регистрация шаблона
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag()  # Позволяет вызвать функцию в шаблоне
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):  # Все контекстные переменные доступны в функции
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- backend/goods/views.py
+++ backend/goods/views.py
@@ -1,44 +1,50 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
-from goods.models import Products 
+from .utils import q_search
+from goods.models import Products
 
 
-def catalog(request, cat_slug):
-    
-    page = request.GET.get('page', 1)
-    on_sale = request.GET.get('on_sale', None)  # имя переменной по атрибуты name в форме
-    order_by = request.GET.get('order_by', None)
-    
-    
-    if cat_slug == 'vse-tovary':
+def catalog(request, cat_slug=None):
+
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get(
+        "on_sale", None
+    )  # имя переменной по атрибуты name в форме
+    order_by = request.GET.get("order_by", None)
+    query = request.GET.get("q", None)
+
+    if cat_slug == "vse-tovary":
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=cat_slug))
-        
+
     if on_sale:
         goods = goods.filter(discount__gt=0)
-        
+
     if order_by and order_by != "default":
         goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)  # Выводим по 3 товара на каждую страницу
-    current_page = paginator.page(int(page))  # Текущая страница, отображаемая пользователю
-    
+    current_page = paginator.page(
+        int(page)
+    )  # Текущая страница, отображаемая пользователю
+
     context = {
         "title": "Home - Каталог",
         "goods": current_page,  # queryset урезан до количества отображаемых элементов. Объект queryset расширяется методами пагинатора (page_range и т.д.)
         "slug_url": cat_slug,
     }
-    
-    
+
     return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
-    
+
     context = {
         "product": product,
     }
-    
+
     return render(request, "goods/product.html", context)
